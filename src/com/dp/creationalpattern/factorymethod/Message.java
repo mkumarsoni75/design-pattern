153c2390449a04b5952c105e5d33d0568a77dc82
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2020-12-31 00:41:59 +0530

    Factory Method implemented

---
+++ src/com/dp/creationalpattern/factorymethod/Message.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.dp.creationalpattern.factorymethod;
+
+/**
+ * @author MITHUN
+ * This class represents interface for our "Product" which is a message.
+ *
+ */
+public abstract class Message {
+	public abstract String getContent();
+	
+	public void addDefaultHeader(){
+		
+	}
+	public void encrypt(){
+		
+	}
+
+}
