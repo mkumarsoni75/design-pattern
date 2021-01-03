1cd42f2f5951a111acd5c84dfdec2ee0fd7f9356
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2021-01-03 23:42:47 +0530

    Type 2

--- src/com/dp/creationalpattern/builder/User.java
+++ src/com/dp/creationalpattern/builder/User.java
@@ -16,5 +16,29 @@ public class User {
 	private String lastName;
 	private LocalDate birthday;
 	private Address address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 	
 }
